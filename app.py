7c6195f17df9360650d96a0272ced1ac899f0a2b
Jajko0 <138450010+TYMONEX@users.noreply.github.com>
2026-07-28 21:24:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,101 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import sqlite3
+from datetime import datetime
 
 app = Flask(__name__)
 
 
-@app.route("/")
+def init_db():
+    conn = sqlite3.connect("comments.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS comments (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        message TEXT,
+        date TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+
+init_db()
+
+
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+
+    if request.method == "POST":
+
+        name = request.form["name"]
+        message = request.form["message"]
+
+
+        if len(name) > 25:
+            name = name[:25]
+
+        if len(message) > 300:
+            message = message[:300]
+
+
+        conn = sqlite3.connect("comments.db")
+        cursor = conn.cursor()
+
+
+        cursor.execute(
+            """
+            INSERT INTO comments
+            (name,message,date)
+            VALUES (?,?,?)
+            """,
+            (
+                name,
+                message,
+                datetime.now().strftime("%d.%m.%Y %H:%M")
+            )
+        )
+
+
+        conn.commit()
+        conn.close()
+
+
+        return redirect("/")
+
+
+
+    conn = sqlite3.connect("comments.db")
+    cursor = conn.cursor()
+
+
+    cursor.execute(
+        """
+        SELECT name,message,date
+        FROM comments
+        ORDER BY id DESC
+        """
+    )
+
+
+    comments = cursor.fetchall()
+
+    conn.close()
+
+
+
+    return render_template(
+        "index.html",
+        comments=comments
+    )
+
 
 
 if __name__ == "__main__":
+
     app.run(
         host="0.0.0.0",
         port=5000
