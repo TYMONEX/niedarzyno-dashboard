07a57d97fe5376f7abbceeda64149711275631ea
Jajko0 <138450010+TYMONEX@users.noreply.github.com>
2026-07-29 22:13:50 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -263,6 +263,11 @@ def admin_logout():
     return redirect(url_for("admin"))
 
 
+@app.get("/mapa")
+def mapa():
+    return render_template("mapa.html")
+
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
